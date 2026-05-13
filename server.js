851127e9a5b342edc5d916df174973723cd419b6
Rodg3rs <rogeriofsilva@gmail.com>
2026-05-13 15:14:55 -0300

    Descrição das melhorias ou correções

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 
 // Conexão com Turso (SQLite remoto)
-const { createClient } = require("@tursodatabase/node");
+const { createClient } = require("@libsql/client");
 const turso = createClient({
   url: process.env.TURSO_URL,
   authToken: process.env.TURSO_TOKEN
